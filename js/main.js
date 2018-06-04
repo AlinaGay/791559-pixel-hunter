f31f775c1884d52f93135f08e134762e415aa938
AlinaGay <gayfullina.a@gmail.com>
2018-06-04 21:15:27 +0300

    Changed whole code according to demo

--- js/main.js
+++ js/main.js
@@ -1,63 +1,65 @@
-//point11
-const templatesId = ['greeting', 'rules', 'game-1', 'game-2', 'game-3', 'stats'];
+'use strict';
 
-//point2
-const main = document.querySelector('main');
+const RIGHT_ARROW = 37;
+const LEFT_ARROW = 39;
 
-//point3
-let screenCount = 0;
+const mouseClickArrows = `<style>
+   .arrows__wrap {
+     position: absolute;
+     top: 95px;
+     left: 50%;
+     margin-left: -56px;
+   }
+   .arrows__btn {
+     background: none;
+     border: 2px solid black;
+     padding: 5px 20px;
+     cursor: pointer;
+   }
+ </style>
+ <button class="arrows__btn"><-</button>
+ <button class="arrows__btn">-></button>`;
 
-const loadTemplate = (templatesId) => {
-  const selector = 'template#' + templatesId;
-  const currentNode = document.querySelector(selector).content.cloneNode(true);
+const mainElement = document.querySelector(`#main`);
 
-  main.innerHTML = '';
-  main.appendChild(currentNode);
+const selectSlide = (element) => {
+  mainElement.innerHTML = ``;
+  mainElement.appendChild(element.cloneNode(true));
 };
 
-//point4
-loadTemplate(templatesId[screenCount]);
+const screens = Array.from(document.querySelectorAll(`template`)).
+map((it) => it.content);
 
-//point5
-const previusScreen = () => {
-  if (screenCount > 0) {
-    loadTemplate(templatesId[--screenCount]);
+let current = 0;
+const select = (index) => {
+  index = index < 0 ? screens.length - 1 : index;
+  index = index >= screens.length ? 0 : index;
+  current = index;
+  selectSlide(screens[current]);
+};
+
+document.addEventListener(`keydown`, (evt) => {
+  switch (evt.keyCode) {
+    case RIGHT_ARROW:
+      select(current + 1);
+      break;
+    case LEFT_ARROW:
+      select(current - 1);
+      break;
   }
-};
+});
 
-const nextScreen = () => {
-  if (screenCount < templatesId.length -1 ) {
-    loadTemplate(templatesId[++screenCount]);
-  }
-};
+select(0);
 
-const uniKeyCode = (event) => {
-  const key = event.keyCode;
-
-  if (key == 37) previusScreen();
-  if (key == 39) nextScreen();
-};
-
-//pont6
 const divElement = document.createElement("div");
+divElement.classList.add(`arrows__wrap`);
+divElement.innerHTML = mouseClickArrows;
 
-divElement.setAttribute("class", "arrows__wrap");
-
-const styleElement = document.createElement("style");
-
-styleElement.innerHTML = ".arrows__wrap {position: absolute;top: 95px;left: 50%;margin-left: -56px;}" +
-  ".arrows__btn {background: none;border: 2px solid black;padding: 5px 20px;}";
-
-const createButton = (innerValue, value) => {
-  const buttonElement = document.createElement("button");
-  buttonElement.setAttribute("class", "arrows__btn");
-  buttonElement.setAttribute("onclick", value);
-  buttonElement.innerHTML = innerValue;
-
-  return buttonElement;
-}
-
-divElement.appendChild(styleElement);
-divElement.appendChild(createButton("<-","previusScreen()"));
-divElement.appendChild(createButton("->","nextScreen()"));
 document.body.appendChild(divElement);
+
+const arrowClickLeft = divElement.children[1];
+const arrowClickRight = divElement.children[2];
+
+arrowClickLeft.addEventListener("click", () => select(current - 1), false);
+arrowClickRight.addEventListener("click", () => select(current + 1), false);
+
