8b588357f812ea0ad48a223b6f2f7c3b2c2e28dd
AlinaGay <gayfullina.a@gmail.com>
2018-06-15 08:53:30 +0300

    Added json object with results of games, function, tests

--- js/data/answers.data.js
+++ js/data/answers.data.js
@@ -1,3 +1,16 @@
+const answersAreNotAllTrue =
+    [{isRight: true, timeInSeconds: 20},
+      {isRight: false, timeInSeconds: 40},
+      {isRight: true, timeInSeconds: 30},
+      {isRight: false, timeInSeconds: 10},
+      {isRight: false, timeInSeconds: 20},
+      {isRight: true, timeInSeconds: 30},
+      {isRight: false, timeInSeconds: 40},
+      {isRight: true, timeInSeconds: 10},
+      {isRight: false, timeInSeconds: 20},
+      {isRight: true, timeInSeconds: 30}
+    ];
+
 const answersAreLessThan10 =
     [
       {isRight: true, timeInSeconds: 20},
@@ -9,4 +22,62 @@ const answersAreLessThan10 =
       {isRight: true, timeInSeconds: 30}
     ];
 
+const answersWithoutSlowAndFast =
+    [{isRight: true, timeInSeconds: 20},
+      {isRight: true, timeInSeconds: 20},
+      {isRight: true, timeInSeconds: 20},
+      {isRight: true, timeInSeconds: 20},
+      {isRight: true, timeInSeconds: 20},
+      {isRight: true, timeInSeconds: 20},
+      {isRight: true, timeInSeconds: 20},
+      {isRight: true, timeInSeconds: 20},
+      {isRight: true, timeInSeconds: 20},
+      {isRight: true, timeInSeconds: 20}
+    ];
+
+const answersWithSlowAndFast =
+    [{isRight: true, timeInSeconds: 20},
+      {isRight: true, timeInSeconds: 5},
+      {isRight: true, timeInSeconds: 5},
+      {isRight: true, timeInSeconds: 20},
+      {isRight: true, timeInSeconds: 20},
+      {isRight: true, timeInSeconds: 45},
+      {isRight: true, timeInSeconds: 30},
+      {isRight: true, timeInSeconds: 50},
+      {isRight: true, timeInSeconds: 20},
+      {isRight: true, timeInSeconds: 20}
+    ];
+
+const answersWithoutSlowAndWithFast =
+    [{isRight: true, timeInSeconds: 20},
+      {isRight: true, timeInSeconds: 5},
+      {isRight: true, timeInSeconds: 5},
+      {isRight: true, timeInSeconds: 20},
+      {isRight: true, timeInSeconds: 20},
+      {isRight: true, timeInSeconds: 20},
+      {isRight: true, timeInSeconds: 20},
+      {isRight: true, timeInSeconds: 20},
+      {isRight: true, timeInSeconds: 20},
+      {isRight: true, timeInSeconds: 20}
+    ];
+
+
+const answersWithSlowAndWithoutFast =
+    [{isRight: true, timeInSeconds: 20},
+      {isRight: true, timeInSeconds: 20},
+      {isRight: true, timeInSeconds: 20},
+      {isRight: true, timeInSeconds: 20},
+      {isRight: true, timeInSeconds: 20},
+      {isRight: true, timeInSeconds: 20},
+      {isRight: true, timeInSeconds: 45},
+      {isRight: true, timeInSeconds: 56},
+      {isRight: true, timeInSeconds: 77},
+      {isRight: true, timeInSeconds: 20}
+    ];
+
+export {answersAreNotAllTrue};
 export {answersAreLessThan10};
+export {answersWithoutSlowAndFast};
+export {answersWithSlowAndFast};
+export {answersWithoutSlowAndWithFast};
+export {answersWithSlowAndWithoutFast};
--- js/data/scoreFunction.test.js
+++ js/data/scoreFunction.test.js
@@ -1,5 +1,6 @@
 import {assert} from 'chai';
-import {answersAreLessThan10} from './answers.data.js';
+import {answersAreNotAllTrue, answersAreLessThan10, answersWithoutSlowAndFast,
+  answersWithSlowAndFast, answersWithoutSlowAndWithFast, answersWithSlowAndWithoutFast} from './answers.data.js';
 import {scoreInTheEnd} from './scoreFunction.js';
 
 describe(`#scoreInTheEnd()`, () => {
@@ -10,5 +11,60 @@ describe(`#scoreInTheEnd()`, () => {
     const failActualValue = scoreInTheEnd(answersAreLessThan10, remainingLives);
 
     assert.equal(failExpectedValue, failActualValue);
+  });
+});
+
+describe(`#scoreInTheEnd()`, () => {
+  it(`should return -1 when the right answers are less than 10`, () => {
+    const failExpectedValue = -1;
+    const remainingLives = 10;
+
+    const failActualValue = scoreInTheEnd(answersAreNotAllTrue, remainingLives);
+
+    assert.equal(failExpectedValue, failActualValue);
+  });
+});
+
+describe(`#scoreInTheEnd()`, () => {
+  it(`should return 1150 when the right answers equal 10 and  time is not fast or slow`, () => {
+    const expectedNotFastNotSlowValue = 1150;
+    const remainingLives = 10;
+
+    const actualNotFastNotSlowValue = scoreInTheEnd(answersWithoutSlowAndFast, remainingLives);
+
+    assert.equal(expectedNotFastNotSlowValue, actualNotFastNotSlowValue);
+  });
+});
+
+describe(`#scoreInTheEnd()`, () => {
+  it(`should return 1100 when the right answers equal 10 and time answer fast, slow and middle`, () => {
+    const expectedFastSlowMiddleValue = 1100;
+    const remainingLives = 10;
+
+    const actualFastSlowMiddleValue = scoreInTheEnd(answersWithSlowAndFast, remainingLives);
+
+    assert.equal(expectedFastSlowMiddleValue, actualFastSlowMiddleValue);
+  });
+});
+
+describe(`#scoreInTheEnd()`, () => {
+  it(`should return 1200 when the right answers equal 10 and time answer is fast and is not slow`, () => {
+    const expectedFastNotSlowValue = 1250;
+    const remainingLives = 10;
+
+    const actualFastNotSlowValue = scoreInTheEnd(answersWithoutSlowAndWithFast, remainingLives);
+
+    assert.equal(expectedFastNotSlowValue, actualFastNotSlowValue);
+  });
+});
+
+describe(`#scoreInTheEnd()`, () => {
+  it(`should return 1000 when the right answers equal 10 and time answer is fast, slow and middle`, () => {
+    const expectedFastSlowMiddleValue = 1000;
+    const remainingLives = 10;
+
+    const actualFastSlowMiddleValue = scoreInTheEnd(answersWithSlowAndWithoutFast, remainingLives);
+
+    assert.equal(expectedFastSlowMiddleValue, actualFastSlowMiddleValue);
   });
 });
